7363948ca01ee1881c0c818255941bb2f0cc3497
Aatif Syed <aatif.syed@metaswitch.com>
2021-06-13 00:05:41 +0100

    test stream

--- src/stream.rs
+++ src/stream.rs
@@ -1,15 +1,14 @@
-use std::fmt::{self, Debug, Formatter};
+use std::fmt::{self, Debug, Display, Formatter};
 use std::io;
 use std::net::SocketAddr;
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
 use async_io::Async;
-use async_std::{io as aio, net::ToSocketAddrs};
-use derive_more::{AsMut, AsRef, Deref, DerefMut};
-use futures::{join, Stream, StreamExt};
+use async_std::net::ToSocketAddrs;
+use derive_more::AsRef;
+use futures::Stream;
 pub use socket_builder::UdpSocketBuilder;
-
 /// Bound socket with buffer
 #[derive(AsRef, Debug)]
 pub struct UdpSocket {
@@ -27,41 +26,106 @@ impl UdpSocket {
     }
 }
 
-impl<'u> Stream for UdpSocket {
-    type Item = ReceivedUdp<'u>;
+impl Stream for UdpSocket {
+    type Item = io::Result<ReceivedUdp>;
 
-    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        todo!()
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let pinned = &mut *self;
+        match pinned.watcher.poll_readable(cx) {
+            Poll::Ready(_ready) => {
+                let sync = pinned.watcher.as_ref();
+                match sync.recv_from(&mut pinned.buffer) {
+                    Ok((len, addr)) => Poll::Ready(Some(Ok(ReceivedUdp {
+                        packet: pinned.buffer[..len].to_vec(),
+                        source: addr,
+                    }))),
+                    Err(e) => Poll::Ready(Some(Err(e))),
+                }
+            }
+            Poll::Pending => Poll::Pending,
+        }
     }
 }
 
-pub struct ReceivedUdp<'s> {
-    pub packet: &'s [u8],
+#[derive(Debug, PartialEq, Eq)]
+pub struct ReceivedUdp {
+    pub packet: Vec<u8>,
     pub source: SocketAddr,
+}
+
+impl Display for ReceivedUdp {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        if let Ok(s) = std::str::from_utf8(&self.packet) {
+            write!(
+                f,
+                "{} bytes from {}: {:?}",
+                self.packet.len(),
+                self.source,
+                s
+            )
+        } else {
+            write!(
+                f,
+                "{} bytes from {}: {:?}",
+                self.packet.len(),
+                self.source,
+                self.packet
+            )
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use async_std::test;
+    use futures::{stream, StreamExt};
+    use itertools::all;
 
     #[test]
     async fn build_socket() {
-        let sock = UdpSocket::builder()
-            .address("localhost:0")
-            .buffer_size(10)
-            .build()
-            .await
-            .unwrap();
+        let sock = UdpSocketBuilder::default().build().await.unwrap();
         println!("{:?}", sock);
         println!("{:?}", sock.as_ref().local_addr());
+    }
+
+    #[test]
+    async fn stream() {
+        let test_data = vec!["hello", "goodbye"];
+        let receiver = UdpSocketBuilder::default().build().await.unwrap();
+        let receiver_address = receiver.as_ref().local_addr().unwrap();
+
+        let sender = &async_std::net::UdpSocket::bind("localhost:0")
+            .await
+            .unwrap();
+        let sender_address = sender.local_addr().unwrap();
+
+        let send = stream::iter(test_data).then(|data| async move {
+            let packet = data.as_bytes();
+            sender.send_to(packet, receiver_address).await.unwrap();
+            let expected = ReceivedUdp {
+                packet: packet.to_vec(),
+                source: sender_address,
+            };
+            println!("Sent {}", data);
+            expected
+        });
+
+        let receive = receiver.map(|r| {
+            let packet = r.unwrap();
+            println!("{}", packet);
+            packet
+        });
+
+        let stream = send.zip(receive).collect::<Vec<_>>().await;
+        assert!(all(stream, |(expected, actual)| expected == actual));
     }
 }
 
 mod socket_builder {
     use super::UdpSocket;
     use async_io::Async;
-    use async_std::{io as aio, net::ToSocketAddrs};
+    use async_std::{io, net::ToSocketAddrs};
     use thiserror::Error;
 
     #[derive(Debug, Error)]
@@ -73,12 +137,21 @@ mod socket_builder {
         #[error("Couldn't convert to socket addresses")]
         BadAddress(std::io::Error),
         #[error("Couldn't bind to socket")]
-        CouldntBind(#[from] aio::Error),
+        CouldntBind(#[from] io::Error),
     }
 
     pub struct UdpSocketBuilder<A: ToSocketAddrs> {
         pub(crate) address: Option<A>,
         pub(crate) buffer: Option<Vec<u8>>,
+    }
+
+    impl Default for UdpSocketBuilder<&str> {
+        fn default() -> Self {
+            Self {
+                address: Some("localhost:0"),
+                buffer: Some(vec![0; 65536]),
+            }
+        }
     }
 
     impl<A: ToSocketAddrs> UdpSocketBuilder<A> {
@@ -96,6 +169,7 @@ mod socket_builder {
             } else if self.buffer.is_none() {
                 return Err(CreateSocketError::NoBufferSize);
             } else {
+                // lifted from async_std
                 let mut last_err = None;
 
                 for addr in self
@@ -118,8 +192,8 @@ mod socket_builder {
 
                 Err(last_err
                     .unwrap_or_else(|| {
-                        aio::Error::new(
-                            aio::ErrorKind::InvalidInput,
+                        io::Error::new(
+                            io::ErrorKind::InvalidInput,
                             "could not resolve to any addresses",
                         )
                     })
